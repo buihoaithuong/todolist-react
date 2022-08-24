f72ec08dd6a97f9db21621d4eec110b9495dc0e8
Hoai Thuong <buihoaithuong14102000@gmail.com>
2022-08-25 00:09:48 +0700

    base commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const mang = JSON.parse(localStorage.getItem('lstCV'))
+  const [listCV, setListCV] = useState(mang ?? [])
+  const [CV, setCV] = useState('')
+
+  const addList = () =>{
+    setListCV( prev =>{
+      const ListCVTemp = [...prev,CV]
+      const CDjson = JSON.stringify(ListCVTemp)
+      localStorage.setItem('lstCV',CDjson)
+      return ListCVTemp
+    })
+    console.log(listCV)
+    setCV('')
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{padding:40}}>
+      <input 
+      value={CV} 
+      onChange={e=> setCV(e.target.value)}>
+
+      </input>
+      <button onClick={addList}>ADD</button>
+
+      <ul>
+        {
+          listCV.map((job, index)=>(
+            <li key={index}>{job}</li>
+          ))
+        }
+      </ul>
     </div>
+
+
   );
 }
 
